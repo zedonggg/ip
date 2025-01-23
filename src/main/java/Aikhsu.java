1e09a22d5f7db1ef8da3ae60f6c006d5717ec85d
Sun <astrolithium@gmail.com>
2025-01-24 01:39:51 +0800

    Level-3

--- src/main/java/Aikhsu.java
+++ src/main/java/Aikhsu.java
@@ -1,51 +1,110 @@
 import java.util.Scanner;
-
 public class Aikhsu {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
         String logo = "____________________________________________________________\n" +
                 " Hello! I'm Aik Hsu\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
 
         Scanner cin = new Scanner(System.in);
         String command = "";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int counter = 0;
 
         while(true) {
             command = cin.nextLine();
-            if (command.equals("bye")) {
-                System.out.println("____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n");
-                break;
-            } else if (command.equals("list")) {
-                if (counter == 0) {
+            String[] segments = command.split(" ");
+
+            switch(segments[0]){
+                case "bye":
                     System.out.println("____________________________________________________________\n" +
-                            "No tasks saved!\n" + "____________________________________________________________\n");
-                } else {
-                    System.out.println("____________________________________________________________\n");
-                    for (int i = 0; i < counter; i++) {
-                        System.out.print(i+1);
-                        System.out.println(". " + tasks[i] + "\n");
+                            "Bye. Hope to see you again soon!\n" +
+                            "____________________________________________________________\n");
+                    cin.close();
+                    return;
+
+                case "list":
+                    if (counter == 0) {
+                        System.out.println("____________________________________________________________\n" +
+                                "No tasks saved!\n" + "____________________________________________________________\n");
+                    } else {
+                        System.out.println("____________________________________________________________\n");
+                        System.out.println("Here are the tasks in your list:\n");
+                        for (int i = 0; i < counter; i++) {
+                            System.out.print(i+1);
+                            System.out.println(". " + tasks[i] + "\n");
+                        }
+                        System.out.println("____________________________________________________________\n");
                     }
-                    System.out.println("____________________________________________________________\n");
-                }
-            } else {
-                tasks[counter] = command;
-                counter += 1;
-                System.out.println("____________________________________________________________\n" +
-                        "added: " + command + "\n" +
-                        "____________________________________________________________\n");
+                    break;
+
+                case "mark":
+                    if (segments.length > 1) {
+                        int id = Integer.parseInt(segments[1]);
+                        if (id >= counter) {
+                            System.out.println("____________________________________________________________\n");
+                            System.out.println("Task does not exist!\n");
+                            System.out.println("____________________________________________________________\n");
+                        } else {
+                            tasks[id].markAsDone();
+                            System.out.println("____________________________________________________________\n");
+                            System.out.println("Nice! I've marked this task as done:\n" + tasks[id] + "\n");
+                            System.out.println("____________________________________________________________\n");
+                        }
+                    }
+                    break;
+
+                case "unmark":
+                    if (segments.length > 1) {
+                        int id = Integer.parseInt(segments[1]);
+                        if (id >= counter) {
+                            System.out.println("____________________________________________________________\n");
+                            System.out.println("Task does not exist!\n");
+                            System.out.println("____________________________________________________________\n");
+                        } else {
+                            tasks[id].markNotDone();
+                            System.out.println("____________________________________________________________\n");
+                            System.out.println("OK, I've marked this task as not done yet:\n" + tasks[id] + "\n");
+                            System.out.println("____________________________________________________________\n");
+                        }
+                    }
+                    break;
+
+                default:
+                    tasks[counter] = new Task(command);
+                    counter += 1;
+                    System.out.println("____________________________________________________________\n" +
+                            "added: " + command + "\n" +
+                            "____________________________________________________________\n");
+
             }
+//            if (command.equals("bye")) {
+//                System.out.println("____________________________________________________________\n" +
+//                        "Bye. Hope to see you again soon!\n" +
+//                        "____________________________________________________________\n");
+//                break;
+//            } else if (command.equals("list")) {
+//                if (counter == 0) {
+//                    System.out.println("____________________________________________________________\n" +
+//                            "No tasks saved!\n" + "____________________________________________________________\n");
+//                } else {
+//                    System.out.println("____________________________________________________________\n");
+//                    for (int i = 0; i < counter; i++) {
+//                        System.out.print(i+1);
+//                        System.out.println(". " + tasks[i] + "\n");
+//                    }
+//                    System.out.println("____________________________________________________________\n");
+//                }
+//            } else {
+//                tasks[counter] = command;
+//                counter += 1;
+//                System.out.println("____________________________________________________________\n" +
+//                        "added: " + command + "\n" +
+//                        "____________________________________________________________\n");
+//            }
         }
 
-        cin.close();
+
     }
 }
